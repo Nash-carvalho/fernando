44341b6c0464de58ba2c5b1c0c162afde5db30bb
Nash-carvalho <feprofeta3@gmail.com>
2025-07-07 13:46:10 -0300

    Add files via upload

---
+++ Fernando-prova.js
@@ -0,0 +1,30 @@
+//1 Variaveis
+let nome = "Fernando Nash"
+let idade = 35
+
+console.log ("meu nome é " +nome)
+console.log ("tenho "    + idade +   " anos ")
+
+
+//2 condicionais
+let nota = 9
+if(nota >= 0 && nota <=4){
+    console.log("recuperação")
+}else if(nota >=5 && nota <=6 ){
+    console.log("reprovado")
+}else if(nota >=7 && nota <=10){
+    console.log("aprovado")
+}
+
+
+//3 loop for
+for (let i = 1; i <=21; i += 2) 
+console.log(i) 
+
+//4 Array
+let frutas = ["banana","maça", "laranja","pera","uva"]
+console.log(frutas[0])
+console.log(frutas[1])
+console.log(frutas[2])
+console.log(frutas[3])
+console.log(frutas[4])
